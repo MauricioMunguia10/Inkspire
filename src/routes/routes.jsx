0809f5b1cbaf6768517b9a42befa935d6ea69328
MauricioMunguia10 <mauriciomunguiagarcia@gmail.com>
2025-01-25 12:27:44 -0600

    add rutas

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,13 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Pagos from "../pages/Tesoreria/Pagos";
+import Home from "../pages/Home/Home";
+
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/tesoreria/pagos" element={<Pagos />} />
     </Routes>
   );
 };
